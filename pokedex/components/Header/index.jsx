83d4c24f6f014fef940b7a7b3db44facb3051f59
Neferu-22230 <luizotavios@gmail.com>
2025-07-31 11:00:56 -0300

    Pokédex40

---
+++ pokedex/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import { Image, Text, View, Pressable } from "react-native";
+import { styles } from "./styles";
+import { Ionicons } from "@expo/vector-icons";
+import { userRouter } from "expo-router";
+
+const imageSourse = require("@/assets/images/header.jpg");
+
+export default function Hearder({ title, back = false }) {
+    const router = useRouter();
+
+    return (
+        <View style={styles.container}>
+        <View style={styles.imageContainer}>
+             <Image source={imageSourse} style={styles.image}/>
+        </View>
+         <View styles={styles.titleContainer}>
+            <View style={{ flexDirection: "row", alignItems: "center"}}>
+                {
+                    back &&
+                    <Pressable onPress={() => router.back()} style={styles.backButton}>
+                        <Ionicons name="arrow-back" size={28} color="black"/>
+                    </Pressable>
+                }
+                <Text style={styles.title}>{title}</Text>
+            </View>
+            <Text style={{ fontSize: 16}}>
+                {!back ? "Encontre seu pokemon favorito aqui." : "Aqui está o seu pokemon"}
+            </Text>
+        </View>
+        </View>
+    );
+}
